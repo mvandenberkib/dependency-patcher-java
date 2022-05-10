4d4b4c0551e7b429d35bcfd7df4c3849b1104218
Martijn van den Berk <m.vandenberk@group-ib.com>
2022-05-10 18:55:47 +0200

    - Patched files can be written correctly to a target directory - Fixed cross-platform file separators - Fixed small issues - Created comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         try {
-            System.out.println("Hello, world!");
             System.out.println("Please enter the path of the directory containing the files to patch in:");
             Scanner scanner = new Scanner(System.in);
             String patchFolderPath = scanner.nextLine();
@@ -20,23 +19,26 @@ public class Main {
                 throw new Exception("The folder (" + patchFolderPath + ") could not be found!");
             }
 
+            /*
+            Get all the .smali files from any of the subdirectories.
+            */
             List<String> foldersToCheck = new ArrayList<>();
             List<String> smaliFiles = new ArrayList<>();
             foldersToCheck.add(patchFolderPath);
-            while(foldersToCheck.size() > 0) {
+            while (foldersToCheck.size() > 0) {
                 String folderToCheck = foldersToCheck.remove(0);
                 File tempFile = new File(folderToCheck);
                 String[] directories = tempFile.list((current, name) -> new File(current, name).isDirectory());
                 String[] files = tempFile.list((current, name) -> new File(current, name).isFile());
-                if(directories != null) {
+                if (directories != null) {
                     for (String directory : directories) {
-                        foldersToCheck.add(folderToCheck + "/" + directory);
+                        foldersToCheck.add(folderToCheck + File.separatorChar + directory);
                     }
                 }
-                if(files != null) {
+                if (files != null) {
                     for (String file : files) {
                         if (file.endsWith(".smali")) {
-                            smaliFiles.add(folderToCheck + "/" + file);
+                            smaliFiles.add(folderToCheck + File.separatorChar + file);
                         }
                     }
                 }
@@ -44,15 +46,18 @@ public class Main {
 
             System.out.println(smaliFiles.size() + " .smali files to patch!");
 
+            /*
+            Extract the class names (and packages) from the .smali files.
+             */
             List<String> classes = new ArrayList<>();
-            for(String filePath : smaliFiles) {
+            for (String filePath : smaliFiles) {
                 File readFile = new File(filePath);
                 Scanner fileReader = new Scanner(readFile);
-                while(fileReader.hasNextLine()) {
+                while (fileReader.hasNextLine()) {
                     String line = fileReader.nextLine();
-                    if(line.startsWith(".class")) {
+                    if (line.startsWith(".class")) {
                         int lIndex = line.indexOf('L');
-                        if(lIndex > 0) {
+                        if (lIndex > 0) {
                             String className = line.substring(lIndex);
                             classes.add(className);
                         }
@@ -63,14 +68,17 @@ public class Main {
 
             System.out.println(classes.size() + " classes to patch.");
 
+            /*
+            Patch the files.
+             */
             int patchedLineCount = 0;
             List<String> patchedFilePaths = new ArrayList<>(classes.size());
-            for(String filePath : smaliFiles) {
+            for (String filePath : smaliFiles) {
                 String patchedFilePath = filePath;
 
-                int prefixIndex = filePath.lastIndexOf('/') + 1;
+                int prefixIndex = filePath.lastIndexOf(File.separatorChar) + 1;
                 int suffixIndex = filePath.lastIndexOf('.');
-                if(prefixIndex >= 0 && suffixIndex >= 0) {
+                if (prefixIndex >= 1 && suffixIndex >= 0) {
                     patchedFilePath = filePath.substring(0, suffixIndex);
                     patchedFilePath += "Patched.smali";
                 }
@@ -78,12 +86,12 @@ public class Main {
                 File readFile = new File(filePath);
                 Scanner fileReader = new Scanner(readFile);
                 List<String> newLines = new ArrayList<>();
-                while(fileReader.hasNextLine()) {
+                while (fileReader.hasNextLine()) {
                     String line = fileReader.nextLine();
 
                     String finalLine = line;
-                    List<String> foundClasses = classes.stream().filter(item -> finalLine.contains(item)).collect(Collectors.toList());
-                    for(String foundClass : foundClasses) {
+                    List<String> foundClasses = classes.stream().filter(finalLine::contains).toList();
+                    for (String foundClass : foundClasses) {
                         String prefixClass = foundClass.substring(0, foundClass.length() - 1);
                         String patchedClass = prefixClass + "Patched;";
                         line = line.replace(foundClass, patchedClass);
@@ -95,14 +103,76 @@ public class Main {
                 fileReader.close();
 
                 File patchedFile = new File(patchedFilePath);
-                patchedFile.createNewFile();
+                if(!patchedFile.createNewFile()){
+                    throw new Exception(patchedFilePath + " already exists!");
+                }
+
                 FileWriter fileWriter = new FileWriter(patchedFile);
-                for(String line : newLines) {
-                    fileWriter.write(line);
+                for (String line : newLines) {
+                    fileWriter.write(line + "\n");
                 }
                 fileWriter.close();
+
+                patchedFilePaths.add(patchedFile.getAbsolutePath());
             }
-        } catch(Exception exc) {
+
+            System.out.println(patchedLineCount + " lines have been altered.");
+
+            System.out.println("Please enter the directory to patch the files into:");
+            String writeDirectoryPath = scanner.nextLine();
+            if (writeDirectoryPath == null || !Files.exists(Path.of(writeDirectoryPath))) {
+                throw new Exception("Invalid directory given!");
+            }
+
+            /*
+            Get the amount of smali folders in the target directory
+             */
+            int smaliCount = 0;
+            File tempFile = new File(writeDirectoryPath);
+            String[] directories = tempFile.list((current, name) -> new File(current, name).isDirectory());
+            if(directories == null || directories.length <= 0) {
+                throw new Exception("No smali directories found!");
+            }
+
+            for (String dir : directories) {
+                if (dir.contains("smali")) {
+                    smaliCount += 1;
+                }
+            }
+
+            /*
+            Copy the patched files to the target directory.
+             */
+            for (String patchedFilePath : patchedFilePaths) {
+                String fileName = patchedFilePath.substring(patchFolderPath.length() + 1);
+                int endSmaliIndex = fileName.indexOf(File.separatorChar);
+                String tempSmali = fileName.substring(0, endSmaliIndex);
+                int numberIndex = tempSmali.indexOf('_');
+                int smaliCounterModifier = 1;
+                if (numberIndex >= 0) {
+                    smaliCounterModifier = Integer.parseInt(tempSmali.substring(13));
+                }
+
+                int subDirIndex = fileName.lastIndexOf(File.separatorChar);
+                String subDir = fileName.substring(0, subDirIndex);
+                int smaliIndex = subDir.indexOf(File.separatorChar);
+                String realSubDir = subDir.substring(smaliIndex);
+
+                String newDir = writeDirectoryPath + File.separatorChar + "smali_classes" + (smaliCount + smaliCounterModifier) + realSubDir;
+                Path newDirPath = Path.of(newDir);
+                if (!Files.exists(newDirPath)) {
+                    Files.createDirectories(newDirPath);
+                }
+
+                int lastSlashIndex = fileName.lastIndexOf(File.separatorChar);
+                Path patchedFilePathPath = Path.of(patchedFilePath);
+                if (lastSlashIndex > 0) {
+                    String shortFileName = fileName.substring(lastSlashIndex);
+                    Files.copy(patchedFilePathPath, Path.of(newDir + shortFileName));
+                    Files.delete(patchedFilePathPath);
+                }
+            }
+        } catch (Exception exc) {
             exc.printStackTrace();
         }
     }
